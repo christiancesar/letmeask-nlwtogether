7897b5339ba75e3559ed751767651d9a2fb9f8a0
christiancesar <christiancnp@gmail.com>
2021-06-22 17:24:54 -0400

    correcoes

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -50,8 +50,9 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     const result = await auth.signInWithPopup(provider);
+
     if (result.user) {
-      const { displayName, photoURL, uid } = result.user;
+      const { displayName, photoURL, uid } = result.user
 
       if (!displayName || !photoURL) {
         throw new Error('Missing information from Google Account.');
